303a618e9b62997961f9761483b0bb29f27f696c
-g <-g>
2025-02-04 12:55:58 -0600

    feat(backedn): add scopes a los modelos de Activo y Tipo

--- app/Models/Activo.php
+++ app/Models/Activo.php
@@ -2,14 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Activo extends Model
 {
     //
     use SoftDeletes;
+
+    //-----scopes-----
+    public function scopeSearch(Builder $query,String $txt):void
+    {
+        $sucQuery = DB::connection('concentradora') // Define la conexión de la otra BD
+        ->table('sucursales')
+        ->select('id_sucursal', 'nombre')
+        ->where('status', 1);
+        $query->where(function($qw)use($txt){
+            $qw->where('serie','LIKE','%'.$txt.'%')
+            ->orWhere('activos.id','LIKE','%'.$txt.'%')
+            ->orWhere('descripcion','LIKE','%'.$txt.'%')
+            ->orWhere('num_equipo','LIKE','%'.$txt.'%')
+            ->orWhereHas('tipos',function(Builder $tipos)use($txt){
+                $tipos->where('nombre','LIKE','%'.$txt.'%');
+            });
+        });
+        //sub join para buscar la sucursal
+        $query->joinSub($sucQuery, 'sucursales', function ($join) {
+            $join->on('activos.sucursal_id', '=', 'sucursales.id_sucursal');
+        })
+        ->orWhere('sucursales.nombre', 'LIKE', "%$txt%");
+    }
+    //----------------
     public function estadodepreciacion()
     {
         return $this->belongsTo(EstadosDepreciacion::class,'estatus_depreciacion', 'id_status');
@@ -21,5 +47,9 @@ class Activo extends Model
     public function puestos():BelongsTo
     {
         return $this->belongsTo(Puesto::class, 'puesto_id', 'id_puesto');
+    }
+    public function sucursal():BelongsTo
+    {
+        return $this->belongsTo(Sucursales::class,'sucursal_id', 'id_sucursal');
     }
 }
--- app/Models/Tipo.php
+++ app/Models/Tipo.php
@@ -2,13 +2,45 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Query\JoinClause;
+use Illuminate\Support\Facades\DB;
 
 class Tipo extends Model
 {
     use HasFactory;
     protected $primaryKey='id_tipo';
+    //-----scopes-----
+    public function scopeSearch(Builder $query,$txt):void
+    {
+        $sucQuery = DB::connection('concentradora') // Define la conexión de la otra BD
+        ->table('sucursales')
+        ->select('id_sucursal', 'nombre as suc_nombre')
+        ->where('status', 1);
+
+        $query->where('nombre','LIKE',"%$txt%")
+        ->orWhere('id_tipo','LIKE',"%$txt%")
+        ->orWhereHas('giro',function(Builder $giro)use($txt){
+            $giro->where('nombre','LIKE',"%$txt%");
+        });
+        $query->joinSub($sucQuery,'sucursales',function(JoinClause $join){
+            $join->on('tipos.sucursal_id', '=', 'sucursales.id_sucursal');
+        })
+        ->orWhere('suc_nombre', 'LIKE', "%$txt%");
+    }
+    //---------------
+    //-----relationships-----
+    public function giro():BelongsTo
+    {
+        return $this->belongsTo(Giro::class,'id_giro','id_giro');
+    }
+    public function sucursal():BelongsTo
+    {
+        return $this->belongsTo(Sucursales::class,'sucursal_id','id_sucursal');
+    }
     /* protected $table = 'tipos';
     public $timestamps=false;
     protected $fillable =[
