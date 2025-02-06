1ad27693aa8e6f45c98f188602e1652968949dca
JoelEstrella06 <joelestrella06@gmail.com>
2025-02-06 13:21:29 -0600

    refactor (back): cambios en funciones para la nueva estructura (modulo de Equi. Computo)

--- app/Http/Controllers/EquipoComputoController.php
+++ app/Http/Controllers/EquipoComputoController.php
@@ -16,20 +16,29 @@ use Illuminate\Support\Facades\Redirect;
 use Barryvdh\DomPDF\Facade\Pdf as PDF;
 use Illuminate\Support\Facades\Validator;
 use App\Exports\EquipoComputoExport;
+use App\Models\Activo;
+use App\Models\Puesto;
+use App\Models\Sucursales;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 
 
 class EquipoComputoController extends Controller
 {
+    private $sucUsers;
     public function __construct()
     {
+		$this->sucUsers=Auth::user()->empleado->sucursales;
     }
     public function index(Request $request)
     {
     	if($request){
     		$querry=trim($request->get('searchText'));
+			$sucursales=$this->sucUsers;
+			$tipos=Tipo::where('id_giro',2)->whereIn('sucursal_id',$sucursales->pluck('id_sucursal'))->get()->pluck('id_tipo');
     		if(auth()->user()->role_id<3){
-				$EquipoComputo=EquipoComputo::leftJoin('tipos', 'equipo_computo.tipo', '=', 'tipos.id_tipo')->
+				/* $EquipoComputo=EquipoComputo::leftJoin('tipos', 'equipo_computo.tipo', '=', 'tipos.id_tipo')->
 				Where(function ($query) use($querry){
 					$query->where('serie','LIKE','%'.$querry.'%')
 					->orWhere('id_equipo_computo','LIKE','%'.$querry.'%')
@@ -37,9 +46,10 @@ class EquipoComputoController extends Controller
 					->orWhere('num_equipo','LIKE','%'.$querry.'%')
 					->orWhere('tipos.nombre','LIKE','%'.$querry.'%');
 				})->select("id_equipo_computo","num_equipo","equipo_computo.estado","descripcion","marca","serie","modelo","tipos.nombre","created_user")
-				->paginate(10);
+				->paginate(10); */
+				$EquipoComputo=Activo::with(['sucursal'=>fn($suc)=>$suc->with('empresa:id_empresa,alias')])->search($querry)->whereIn('sucursal_id',$sucursales->pluck('id_sucursal'))->whereIn('tipo_id',$tipos)->orderBy('id','ASC')->paginate(10);
 			}else{
-				$EquipoComputo=EquipoComputo::leftJoin('tipos', 'equipo_computo.tipo', '=', 'tipos.id_tipo')->
+				/* $EquipoComputo=EquipoComputo::leftJoin('tipos', 'equipo_computo.tipo', '=', 'tipos.id_tipo')->
 				where('created_user', '=', auth()->user()->id)
 				->Where(function ($query) use($querry){
 					$query->where('serie','LIKE','%'.$querry.'%')
@@ -48,30 +58,32 @@ class EquipoComputoController extends Controller
 					->orWhere('num_equipo','LIKE','%'.$querry.'%')
 					->orWhere('tipos.nombre','LIKE','%'.$querry.'%');
 				})->select("id_equipo_computo","num_equipo","equipo_computo.estado","descripcion","marca","serie","modelo","tipos.nombre","created_user")
-				->paginate(10);
+				->paginate(10); */
 			}
     		return view('inventario.EquipoComputo.index',["EquipoComputo"=>$EquipoComputo,"searchText"=>$querry]);
     	}
     }
     public function create()
     {
-        $Departamento=Departamentos::where('estado','=','1')->orderBy('nombre')->get();
-        $Puesto=Puestos::where('estado','=','1')->orderBy('nombre')->get();
-        $Empleado=Empleado::where('estado','=','1')->orderBy('apellido_paterno')->get();
-    	$tipos=Tipo::where('estado','=','1')->where('id_giro','=','2')->get();
+		$sucursales=$this->sucUsers;
+        $Departamento=Departamentos::where('status','=','1')->orderBy('nombre')->get();
+        //$Puesto=Puestos::where('estado','=','1')->orderBy('nombre')->get();
+		$Puesto=Puesto::join('departamento_puesto as dp','puestos.id_puesto','=','dp.id_puesto')->select('puestos.*','dp.id_departamento')->get();
+        $Empleado=Empleado::where('status','=','1')->orderBy('apellido_p')->get();
+    	$tipos=Tipo::where('estado','=','1')->where('id_giro','=','2')->whereIn('sucursal_id',$sucursales->pluck('id_sucursal'))->get();
 		$depreciacion=EstadosDepreciacion::where('estado','=','1')->get();
-    	return view("inventario.EquipoComputo.create",["tipos"=>$tipos,"Departamento"=>$Departamento,"Puesto"=>$Puesto,"Empleado"=>$Empleado, "Depreciacion"=>$depreciacion]);
+    	return view("inventario.EquipoComputo.create",["tipos"=>$tipos,"Departamento"=>$Departamento,"Puesto"=>$Puesto,"Empleado"=>$Empleado, "Depreciacion"=>$depreciacion,"sucursales"=>$sucursales]);
     }
     public function store(EquipoComputoRequest $request)
     {					  
-    	$EquipoComputo= new EquipoComputo;
+    	$EquipoComputo= new Activo();
     	$EquipoComputo->estado='1';
     	$EquipoComputo->descripcion=$request->get('descripcion');
     	$EquipoComputo->marca=$request->get('marca');
     	$EquipoComputo->serie=$request->get('serie');
     	$EquipoComputo->modelo=$request->get('modelo');
     	$EquipoComputo->color=$request->get('color');
-    	$EquipoComputo->tipo=$request->get('tipo');
+    	$EquipoComputo->tipo_id=$request->get('tipo');
         $EquipoComputo->costo=$request->get('costo');
     	$EquipoComputo->nombre_provedor=$request->get('nombre_provedor');
 		$EquipoComputo->id_proveedor=$request->get('id_proveedor');
@@ -82,48 +94,54 @@ class EquipoComputoController extends Controller
 		$EquipoComputo->fecha_depreciacion_inicio=$request->get('fecha_depreciacion_inicio');
     	$EquipoComputo->tasa_depreciacion=$request->get('tasa_depreciacion');
     	$EquipoComputo->vida_util=$request->get('vida_util');
-    	$EquipoComputo->area_destinada=$request->get('area_destinada');
-    	$EquipoComputo->puesto=$request->get('puesto');
-    	$EquipoComputo->nombre_responsable=$request->get('nombre_responsable');
+    	$EquipoComputo->departamento_id=$request->get('area_destinada');
+    	$EquipoComputo->puesto_id=$request->get('puesto');
+    	$EquipoComputo->responsable_id=$request->get('nombre_responsable');
        	$EquipoComputo->observaciones=$request->get('observaciones');
     	$EquipoComputo->garantia=$request->get('garantia');
         $EquipoComputo->created_user=auth()->user()->id;
+		$EquipoComputo->sucursal_id=$request->get('sucursal_origen');
     	$EquipoComputo->save();
     	if($request->hasFile('xml')){
     		$file=$request->file('xml');
-    		$file->move(public_path().'/archivos/inventario/EquipoComputo/xml/',$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName());
-    		$EquipoComputo->xml=$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName();
+    		$file->storeAs('/archivos/inventario/EquipoComputo/xml/',$EquipoComputo->id."-".$file->getClientOriginalName(),'public');
+    		//$file->move(public_path().'/archivos/inventario/EquipoComputo/xml/',$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName());
+    		$EquipoComputo->xml=$EquipoComputo->id."-".$file->getClientOriginalName();
     	}  	
     	if($request->hasFile('pdf')){
     		$file=$request->file('pdf');
-    		$file->move(public_path().'/archivos/inventario/EquipoComputo/pdf/',$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName());
-    		$EquipoComputo->pdf=$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName();
+    		$file->storeAs('/archivos/inventario/EquipoComputo/pdf/',$EquipoComputo->id."-".$file->getClientOriginalName(),'public');
+    		//$file->move(public_path().'/archivos/inventario/EquipoComputo/pdf/',$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName());
+    		$EquipoComputo->pdf=$EquipoComputo->id."-".$file->getClientOriginalName();
     	}
-		$id=$EquipoComputo->id_equipo_computo;
+		$id=$EquipoComputo->id;
     	if($request->hasFile('foto')) {
             foreach($request->file('foto') as $image) {
                 $Imagen= new imagen;
-                $image->move(public_path().'/imagenes/inventario/EquipoComputo/img/',$id."_".time()."_".$image->getClientOriginalName());
-                $Imagen->id_equipo=$id;
-                $Imagen->id_area='2';
+    		    $image->storeAs('/imagenes/inventario/EquipoComputo/img/',$id."_".time()."_".$image->getClientOriginalName(),'public');
+                //$image->move(public_path().'/imagenes/inventario/EquipoComputo/img/',$id."_".time()."_".$image->getClientOriginalName());
+                $Imagen->activo_id=$id;
+                //$Imagen->id_area='2';
                 $Imagen->imagen=$id."_".time()."_".$image->getClientOriginalName();
     	        $Imagen->estado='1';
                 $image->getClientOriginalName();
                 $Imagen->save();
             }
         }
-    	$EquipoComputo->num_equipo='EQC-'.$EquipoComputo->id_equipo_computo;
+		$tipos=Tipo::where('id_giro',2)->where('sucursal_id',$request->get('sucursal_origen'))->get()->pluck('id_tipo');
+        $cont=Activo::where('sucursal_id',$request->get('sucursal_origen'))->whereIn('tipo_id',$tipos)->get()->count();
+    	$EquipoComputo->num_equipo='EQC-'.($cont+1);
 		$EquipoComputo->update();
     	return Redirect::to('admin/inventario/EquipoComputo');
     }
     public function show($id)
     {
-        $Departamento=Departamentos::where('estado','=','1')->get();
-        $Puesto=Puestos::get();
+        $Departamento=Departamentos::where('status','=','1')->get();
+        $Puesto=Puesto::get();
     	$Empleado=Empleado::get();
-    	$EquipoComputo=EquipoComputo::findOrFail($id);
+    	$EquipoComputo=Activo::findOrFail($id);
     	$tipos=Tipo::where('estado','=','1')->where('id_giro','=','2')->get();
-		$Imagen=Imagen::where('estado','=','1')->where('id_equipo','=',$id)->where('id_area', '=', 2)->get();
+		$Imagen=Imagen::where('estado','=','1')->where('activo_id','=',$id)->get();
 		$depreciacion=EstadosDepreciacion::where('estado','=','1')->get();
 		if(auth()->user()->role_id<3 || auth()->user()->id == $EquipoComputo->created_user){
 			return view("inventario.EquipoComputo.show",["EquipoComputo"=>$EquipoComputo,"tipos"=>$tipos,"Departamento"=>$Departamento,"Puesto"=>$Puesto,"Empleado"=>$Empleado,"Imagen"=>$Imagen, "Depreciacion"=>$depreciacion]);
@@ -133,28 +151,29 @@ class EquipoComputoController extends Controller
     }
     public function edit($id)
     {
-		$EquipoComputo=EquipoComputo::findOrFail($id);
-		$Departamento=Departamentos::where('estado','=','1')->get();
-		$Puesto=Puestos::where('estado','=','1')->orderBy('nombre')->get();
-		$Empleado=Empleado::where('estado','=','1')->orderBy('apellido_paterno')->get();
-		$tipos=Tipo::where('estado','=','1')->where('id_giro','=','2')->get();
+		$sucursales=$this->sucUsers;
+		$EquipoComputo=Activo::findOrFail($id);
+		$Departamento=Departamentos::where('status','=','1')->get();
+		$Puesto=Puesto::where('status','=','1')->orderBy('nombre')->get();
+		$Empleado=Empleado::where('status','=','1')->orderBy('apellido_p')->get();
+		$tipos=Tipo::where('estado','=','1')->where('id_giro','=','2')->where('sucursal_id',$EquipoComputo->sucursal_id)->get();
 		$depreciacion=EstadosDepreciacion::where('estado','=','1')->get();
-		$Imagen=Imagen::where('estado','=','1')->where('id_equipo','=',$id)->where('id_area', '=', 2)->get();
+		$Imagen=Imagen::where('estado','=','1')->where('activo_id','=',$id)->get();
 		if(auth()->user()->role_id<3 || auth()->user()->id == $EquipoComputo->created_user){
-			return view("inventario.EquipoComputo.edit",["EquipoComputo"=>$EquipoComputo,"tipos"=>$tipos,"Departamento"=>$Departamento,"Puesto"=>$Puesto,"Empleado"=>$Empleado,"Imagen"=>$Imagen, "Depreciacion"=>$depreciacion]);  
+			return view("inventario.EquipoComputo.edit",["EquipoComputo"=>$EquipoComputo,"tipos"=>$tipos,"Departamento"=>$Departamento,"Puesto"=>$Puesto,"Empleado"=>$Empleado,"Imagen"=>$Imagen, "Depreciacion"=>$depreciacion,"sucursales"=>$sucursales]);  
 		}else{
 			return redirect('/admin/inventario/EquipoComputo');
 		}
     }
     public function update(EquipoComputoRequest $request, $id)
     {
-    	$EquipoComputo = EquipoComputo::findOrFail($id);
+    	$EquipoComputo = Activo::findOrFail($id);
     	$EquipoComputo->descripcion=$request->get('descripcion');
     	$EquipoComputo->marca=$request->get('marca');
     	$EquipoComputo->serie=$request->get('serie');
     	$EquipoComputo->modelo=$request->get('modelo');
     	$EquipoComputo->color=$request->get('color');
-    	$EquipoComputo->tipo=$request->get('tipo');
+    	$EquipoComputo->tipo_id=$request->get('tipo');
         $EquipoComputo->costo=$request->get('costo');
     	$EquipoComputo->nombre_provedor=$request->get('nombre_provedor');
 		$EquipoComputo->id_proveedor=$request->get('id_proveedor');
@@ -175,35 +194,40 @@ class EquipoComputoController extends Controller
     	if($request->get('motivo_baja')!=""){
 			$EquipoComputo->motivo_baja=$request->get('motivo_baja');
     	}
-    	$EquipoComputo->area_destinada=$request->get('area_destinada');
-    	$EquipoComputo->puesto=$request->get('puesto');
-    	$EquipoComputo->nombre_responsable=$request->get('nombre_responsable');
+    	$EquipoComputo->departamento_id=$request->get('area_destinada');
+    	$EquipoComputo->puesto_id=$request->get('puesto');
+    	$EquipoComputo->responsable_id=$request->get('nombre_responsable');
     	$EquipoComputo->observaciones=$request->get('observaciones');
     	$EquipoComputo->garantia=$request->get('garantia');
 		$EquipoComputo->precio_venta=$request->get('precio_venta');
 		$EquipoComputo->estatus_depreciacion=$request->get('estatus_depreciacion');
+		$EquipoComputo->sucursal_id=$request->get('sucursal_origen');
     	if($request->hasFile('xml')){
     		$file=$request->file('xml');
-    		$file->move(public_path().'/archivos/inventario/EquipoComputo/xml/',$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName());
-    		$EquipoComputo->xml=$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName();
+    		$file->storeAs('/archivos/inventario/EquipoComputo/xml/',$EquipoComputo->id."-".$file->getClientOriginalName(),'public');
+    		//$file->move(public_path().'/archivos/inventario/EquipoComputo/xml/',$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName());
+    		$EquipoComputo->xml=$EquipoComputo->id."-".$file->getClientOriginalName();
     	}
     	if($request->hasFile('pdf')){
     		$file=$request->file('pdf');
-    		$file->move(public_path().'/archivos/inventario/EquipoComputo/pdf/',$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName());
-    		$EquipoComputo->pdf=$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName();
+    		$file->storeAs('/archivos/inventario/EquipoComputo/pdf/',$EquipoComputo->id."-".$file->getClientOriginalName(),'public');
+    		//$file->move(public_path().'/archivos/inventario/EquipoComputo/pdf/',$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName());
+    		$EquipoComputo->pdf=$EquipoComputo->id."-".$file->getClientOriginalName();
     	}
     	if($request->hasFile('carta_responsiva')){
     		$file=$request->file('carta_responsiva');
-    		$file->move(public_path().'/archivos/inventario/EquipoComputo/carta_responsiva/',$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName());
-    		$EquipoComputo->carta_responsiva=$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName();
+    		$file->storeAs('/archivos/inventario/EquipoComputo/carta_responsiva/',$EquipoComputo->id."-".$file->getClientOriginalName(),'public');
+    		//$file->move(public_path().'/archivos/inventario/EquipoComputo/carta_responsiva/',$EquipoComputo->id_equipo_computo."-".$file->getClientOriginalName());
+    		$EquipoComputo->carta_responsiva=$EquipoComputo->id."-".$file->getClientOriginalName();
     	}
 		$EquipoComputo->update();
 		if($request->hasFile('foto')) {
             foreach($request->file('foto') as $image) {
                 $Imagen= new imagen;
-                $image->move(public_path().'/imagenes/inventario/EquipoComputo/img/',$id."_".time()."_".$image->getClientOriginalName());
-                $Imagen->id_equipo=$id;
-                $Imagen->id_area='2';
+    		    $image->storeAs('/imagenes/inventario/EquipoComputo/img/',$id."_".time()."_".$image->getClientOriginalName(),'public');
+                //$image->move(public_path().'/imagenes/inventario/EquipoComputo/img/',$id."_".time()."_".$image->getClientOriginalName());
+                $Imagen->activo_id=$id;
+                //$Imagen->id_area='2';
                 $Imagen->imagen=$id."_".time()."_".$image->getClientOriginalName();
     	        $Imagen->estado='1';
                 $image->getClientOriginalName();
@@ -214,7 +238,7 @@ class EquipoComputoController extends Controller
     }
     public function destroy(Request $request,$id)
     {
-    	$EquipoComputo = EquipoComputo::findOrFail($id);
+    	$EquipoComputo = Activo::findOrFail($id);
     	$EquipoComputo->estado='0';
     	$now = new \DateTime();
 		$EquipoComputo->fecha_baja= $now->format('y-m-d');
@@ -225,15 +249,15 @@ class EquipoComputoController extends Controller
     public function activar(Request $request)
     {
         $id =$request->get('id_act');
-        $EquipoComputo = EquipoComputo::findOrFail($id);
+        $EquipoComputo = Activo::findOrFail($id);
         $EquipoComputo->estado='1';   
         $EquipoComputo->update();
         return Redirect::to('admin/inventario/EquipoComputo');
     }
     public function reporte(){
-        $Departamento=Departamentos::where('estado','=','1')->get();
-        $Puesto=Puestos::get();
-        $Empleado=Empleado::get();
+        $Departamento=Departamentos::where('status','=','1')->get();
+        $Puesto=Puesto::where('status',1)->get();
+        $Empleado=Empleado::where('status',1)->get();
     	$tipos=Tipo::where('estado','=','1')->where('id_giro','=','2')->get();
     	return view("inventario.EquipoComputo.reporte",["tipos"=>$tipos,"Departamento"=>$Departamento,"Puesto"=>$Puesto,"Empleado"=>$Empleado]);
     }
@@ -242,20 +266,22 @@ class EquipoComputoController extends Controller
     }
     public function responsiva($id){
          // responsiva para unico equipo
-	    $EquipoComputo = EquipoComputo::findOrFail($id);
+	    $EquipoComputo = Activo::findOrFail($id);
         $pdf = PDF::loadView('pdf.EquipoComputo', compact('EquipoComputo'));
 		return $pdf->stream('carta_responsiva_'.$id.'.pdf');
     }
 	public function responsivas(){
            //devuelve vista para elegir a la persona
-        $Empleado=Empleado::where('estado','=','1')->orderBy('apellido_paterno')->get();
+        $Empleado=Empleado::where('status','=','1')->orderBy('apellido_p')->get();
     	return view("inventario.EquipoComputo.responsivas",["Empleado"=>$Empleado]);
     }
 	public function imprimir_responsiva(Request $request){
          //responsiva para todos los equipos de una persona
+		 //dd($request);
 		$id =$request->get('nombre_responsable');
+		$tipos=Tipo::where('id_giro',2)->get()->pluck('id_tipo');
 		$Empleado=Empleado::findOrFail($id);
-		$EquipoComputo = DB::select('select * from equipo_computo where estado=1 and nombre_responsable='.$id);
+		$EquipoComputo =Activo::where([['estado',1],['responsable_id',$id]])->whereIn('tipo_id',$tipos)->get(); 
 		$pdf = PDF::loadView('pdf.Responsiva', ["EquipoComputo"=>$EquipoComputo,"Empleado"=>$Empleado]);
 		return $pdf->stream('carta_responsiva_.pdf');
 	}
@@ -271,8 +297,9 @@ class EquipoComputoController extends Controller
 				$id=$obj->id_imagen;
 				$img = Imagen::find($id);
 				$img->delete();
-				$path=public_path().'/imagenes/inventario/EquipoComputo/img/'.$obj->imagen;
-				unlink($path);
+				Storage::disk('public')->delete('/imagenes/inventario/EquipoTransporte/img/'.$obj->imagen);
+				//$path=public_path().'/imagenes/inventario/EquipoComputo/img/'.$obj->imagen;
+				//unlink($path);
 				$result=true;
 			}
 		} 
--- app/Http/Requests/EquipoComputoRequest.php
+++ app/Http/Requests/EquipoComputoRequest.php
@@ -38,7 +38,8 @@ class EquipoComputoRequest extends FormRequest
             'area_destinada'=>'required|numeric',
             'puesto'=>'required|numeric',
             'nombre_responsable'=>'required|numeric',
-            'precio_venta'=>'numeric|nullable'
+            'precio_venta'=>'numeric|nullable',
+            'sucursal_origen'=>'required'
         ];
     }
 }
