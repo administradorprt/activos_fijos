0a8314eb666b541100c8d94302af84dd9c4613d5
JoelEstrella06 <joelestrella06@gmail.com>
2025-02-13 16:14:59 -0600

    feat(back): add relaciones entre modelos

--- app/Models/ManteActivo.php
+++ app/Models/ManteActivo.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ManteActivo extends Model
 {
-    //
+    use SoftDeletes;
+
+    //---scopes---
+
+    //------------
+    public function activo():BelongsTo
+    {
+        return $this->belongsTo(Activo::class);
+    }
+    public function frecuencia():BelongsTo
+    {
+        return $this->belongsTo(FrecuenciaMantenimiento::class);
+    }
+    public function mantenimientos():HasMany
+    {
+        return $this->hasMany(Mantenimiento::class);
+    }
+    //obtener último mantenimiento por relación
+    public function lastMante():HasOne
+    {
+        return $this->hasOne(Mantenimiento::class)->latest('id');
+    }
 }
--- app/Models/Mantenimiento.php
+++ app/Models/Mantenimiento.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Mantenimiento extends Model
 {
-    //
+    use SoftDeletes;
+
+    public function archivos():HasMany
+    {
+        return $this->hasMany(MantenimientoArchivo::class);
+    }
+    //obtener archivos pdf
+    public function pdfs():HasMany
+    {
+        return $this->hasMany(MantenimientoArchivo::class)->where('extension', 'pdf');
+    }
+    //obtener imagenes según su mime type
+    public function imagenes():HasMany
+    {
+        return $this->hasMany(MantenimientoArchivo::class)->whereIn('type', [
+            'image/jpeg', 'image/png', 'image/gif', 'image/webp', 
+            'image/svg+xml', 'image/avif', 'image/heic', 'image/heif'
+        ]);
+    }
 }
