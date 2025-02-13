8c67bc4b1fc86574760ae9d52e901f57465cb61b
JoelEstrella06 <joelestrella06@gmail.com>
2025-02-13 16:17:25 -0600

    feat(back): función para registrar mantenimeintos de un activo

--- app/Http/Controllers/MantenimientosController.php
+++ app/Http/Controllers/MantenimientosController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Activo;
 use App\Models\FrecuenciaMantenimiento;
 use App\Models\ManteActivo;
+use App\Models\Mantenimiento;
+use App\Models\MantenimientoArchivo;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
@@ -17,32 +19,64 @@ class MantenimientosController extends Controller
     {
 		$this->sucUsers=Auth::user()->empleado->sucursales;
     }
-    /* public function index($tipo){
-        return view('inventario.Mantenimientos.index');
-    } */
-    public function create($tipo){
-      $sucursales=$this->sucUsers;
-      $frecuencias=FrecuenciaMantenimiento::all();
-       return view('inventario.Mantenimientos.create',compact('sucursales','frecuencias'));
+    public function create(ManteActivo $activo){
+       return view('inventario.Mantenimientos.create',compact('activo'));
     }
-    public function store(Request $request){
+    public function store(Request $request,ManteActivo $activo){
+      //dd($activo);
       $request->validate([
-        'sucursal_origen' =>'required',
-        'activo' =>'required',
-        'periodo' =>'required',
+        'tipo' =>'required',
+        'proveedor' =>'required',
+        'fecha' =>'required|date',
+        'pdfs.*'=>'required|mimes:pdf',
+        'foto.*' =>'mimes:jpg,bmp,png,jpegd',
       ]);
+      //dd($activo->activo->tipos->giro->nombre);
       try {
-        $fecha=Carbon::now()->toDateString();
-        $mante=new ManteActivo();
-        $mante->activo_id=$request->activo;
-        $mante->frecuencia_id=$request->periodo;
-        $mante->ultimo_mante=$fecha;
-        $mante->proximo_mante=$fecha;
-        $mante->status=true;
+        $mante=new Mantenimiento();
+        $mante->mante_activo_id=$activo->id;
+        $mante->activo_id=$activo->activo_id;
+        $mante->tipo=$request->tipo;
+        $mante->proveedor=$request->proveedor;
+        $mante->fecha=$request->fecha;
+        $mante->comentarios=$request->comentario;
         $mante->save();
-        return to_route('mante',5);
+        if($request->hasFile('pdfs')){
+          foreach($request->file('pdfs') as $pdf){
+            $path=$pdf->storeAs('/mantenimientos/pdfs/'.$activo->activo->tipos->giro->nombre.'/',$mante->id."_".time()."_".$pdf->getClientOriginalName(),'public');
+            $archivo=new MantenimientoArchivo();
+            $archivo->mantenimiento_id=$mante->id;
+            $archivo->name=$pdf->getClientOriginalName();
+            $archivo->extension=$pdf->getClientOriginalExtension();
+            $archivo->type=$pdf->getMimeType();
+            $archivo->path=$path;
+            $archivo->save();
+          }
+        }
+        if($request->hasFile('foto')){
+          foreach($request->file('foto') as $foto){
+            $path=$foto->storeAs('/mantenimientos/fotos/'.$activo->activo->tipos->giro->nombre.'/',$mante->id."_".time()."_".$foto->getClientOriginalName(),'public');
+            $archivo=new MantenimientoArchivo();
+            $archivo->mantenimiento_id=$mante->id;
+            $archivo->name=$foto->getClientOriginalName();
+            $archivo->extension=$foto->getClientOriginalExtension();
+            $archivo->type=$foto->getMimeType();
+            $archivo->path=$path;
+            $archivo->save();
+          }
+        }
+        $activo->ultimo_mante=$request->fecha;
+        $activo->proximo_mante=Carbon::create($request->fecha)->addDays($activo->frecuencia->dias)->toDateString();
+        $activo->update();
+        return to_route('mante',$activo->activo->tipos->id_giro);
       } catch (Exception $e) {
-        return back()->with('error', 'Error al guardar el mantenimiento: '.$e->getMessage());
+        return back()->withInput()->withErrors(['error'=> 'Error al guardar el mantenimiento: '.$e->getMessage()]);
       }
+    }
+    public function updateProxMante(Request $request, ManteActivo $activo) {
+      $request->validate(['fecha'=>'required']);
+      $activo->proximo_mante=$request->fecha;
+      $activo->update();
+      return to_route('mante',$activo->activo->tipos->id_giro);
     }
 }
