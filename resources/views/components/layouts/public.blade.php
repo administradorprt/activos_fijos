087c97e561ddb5a9ad0ee6907d8751ea5636c845
JoelEstrella06 <joelestrella06@gmail.com>
2025-02-20 17:59:36 -0600

    feat(front): add layout público

---
+++ resources/views/components/layouts/public.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ES">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Gestor de gastos</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <body class="bg-gray-100">
+        <main class="max-w-7xl m-auto pt-7 pb-5 px-2 sm:px-0">
+            {{$slot}}
+        </main>
+    </body>
+</html>
