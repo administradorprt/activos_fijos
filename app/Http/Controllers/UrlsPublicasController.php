31755ef3231e644a725009bbcfb3951c094a5570
JoelEstrella06 <joelestrella06@gmail.com>
2025-02-20 17:52:47 -0600

    feat(front): add controller para rutas publicas

---
+++ app/Http/Controllers/UrlsPublicasController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ManteActivo;
+use Illuminate\Http\Request;
+
+class UrlsPublicasController extends Controller
+{
+  public function publicShow(Request $request,ManteActivo $mante){
+    //validamos que la ruta esté firmada
+    if(! $request->hasValidSignature()){
+      abort(401);
+    }
+    $mante->load('activo','lastMante');
+    return view('pages.public.publicShow',compact('mante'));
+  }
+  public function historico(Request $request,ManteActivo $activo) {
+    //validamos que la ruta esté firmada
+    if(!$request->hasValidSignature()){
+      abort(401);
+    }
+    $activo->load([
+      'lastMante'=>fn($last)=>$last->with('imagenes','pdfs'),
+      'mantenimientos'=>fn($mantos)=>$mantos->select('id','mante_activo_id','fecha')->orderBy('fecha','DESC')
+    ]);
+    return view('pages.public.historial-mantenimiento',compact('activo'));
+  }
+
+}
