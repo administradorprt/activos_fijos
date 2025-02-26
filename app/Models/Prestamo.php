290032f91164e9f0e481a797cc5d14206230643c
JoelEstrella06 <joelestrella06@gmail.com>
2025-02-26 10:52:30 -0600

    feat(back): add relaciones en modelo de prestamo

--- app/Models/Prestamo.php
+++ app/Models/Prestamo.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Prestamo extends Model
 {
-    //
+    use SoftDeletes;
+    
+    public function activo():BelongsTo
+    {
+        return $this->belongsTo(Activo::class);
+    }
+    public function usuario():BelongsTo
+    {
+        return $this->belongsTo(Empleado::class,'user_id','id_empleado');
+    }
 }
