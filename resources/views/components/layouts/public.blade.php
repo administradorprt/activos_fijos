90ded5abc1e3aec92223019712f0ea32b359b0a4
JoelEstrella06 <joelestrella06@gmail.com>
2025-02-27 11:28:09 -0600

    style: cambios de texto en head de app

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Gestor de gastos</title>
+        <title>Activos fijos</title>
         <link rel="stylesheet" href="{{asset('css/spinner.css')}}">
         <link rel="stylesheet" href="{{asset('css/acordeon.css')}}">
         <link rel="stylesheet" href="{{asset('css/modal.css')}}">
