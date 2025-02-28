6516dcc60e26481c97884fbc24cc0fd761ec0ad1
JoelEstrella06 <joelestrella06@gmail.com>
2025-02-28 16:54:51 -0600

    feat(back): add relaciones a los modelos

--- app/Models/Cajon.php
+++ app/Models/Cajon.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cajon extends Model
 {
+    use SoftDeletes;
     protected $table="cajones";
+
+    public function activos():BelongsToMany
+    {
+        return $this->belongsToMany(Activo::class,'activos_carritos')->wherePivotNull('deleted_at');
+    }
+    public function carrito():BelongsTo
+    {
+        return $this->belongsTo(Carrito::class);
+    }
 }
--- app/Models/Carrito.php
+++ app/Models/Carrito.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Carrito extends Model
 {
-    //
+    use SoftDeletes;
+
+    public function sucursal():BelongsTo
+    {
+        return $this->belongsTo(Sucursales::class,'sucursal_id','id_sucursal');
+    }
+    public function asignado():BelongsTo
+    {
+        return $this->belongsTo(Empleado::class,'user_asignado','id_empleado');
+    }
+    public function cajones():HasMany
+    {
+        return $this->hasMany(Cajon::class);
+    }
 }
